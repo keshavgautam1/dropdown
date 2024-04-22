be4aeec80370081aa3d04a0ca2fe1ed9a96853b1
Keshav Gautam <keshavgautam375@gmail.com>
2024-04-22 13:44:11 +0530

    Remove unused test files and dependencies, and update tailwindcss version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Dropdown from "./component/Dropdown";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen grid place-items-center bg-gradient-to-r from-sky-600 to-indigo-600">
+      <Dropdown />
     </div>
   );
 }
---
+++ src/component/Dropdown.js
@@ -0,0 +1,79 @@
+// import { useState } from "react";
+// import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
+// import list from "../list.json";
+
+// function Dropdown() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   // return (
+//   //   <div className="relative top-20 flex flex.col items-center w-[340px] h-[450px] rounded-lg">
+//   //     <div className="relative top-1/4 flex flex-col items-center">
+//   //       <button
+//   //         onClick={() => setIsOpen((prev) => !prev)}
+//   //         className="bg-blue-400 p-4 w-full flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
+//   //       >
+//   //         Dropdown
+//   //         {!isOpen ? (
+//   //           <AiOutlineCaretDown className="h-8" />
+//   //         ) : (
+//   //           <AiOutlineCaretUp className="h-8" />
+//   //         )}
+//   //       </button>
+
+//   //       {isOpen && (
+//   //         <div className="bg-blue-400 mt-1 flex flex-col items-start rounded-lg p-2 w-full">
+//   //           {list.map((item, i) => (
+//   //             <div
+//   //               key={i}
+//   //               className="flex w-full justify-between hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent"
+//   //             >
+//   //               <h3 className="font-bold">{item.city}</h3>
+//   //               <h3>{item.emoticon}</h3>
+//   //             </div>
+//   //           ))}
+//   //         </div>
+//   //       )}
+//   //     </div>
+//   //   </div>
+//   // );
+
+// export default Dropdown;
+
+import { useState } from "react";
+import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
+import list from "../list.json";
+
+function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="relative">
+        <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="bg-blue-400 p-4 w-[340px] flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
+        >
+          Dropdown
+          {!isOpen ? (
+            <AiOutlineCaretDown className="h-8" />
+          ) : (
+            <AiOutlineCaretUp className="h-8" />
+          )}
+        </button>
+        {isOpen && (
+          <div className="absolute top-full mt-2 bg-blue-400 flex flex-col items-start rounded-lg p-2 w-[340px]">
+            {list.map((item, i) => (
+              <div
+                key={i}
+                className="flex w-full justify-between hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent"
+              >
+                <h3>{item.city}</h3>
+                <h3>{item.emoticon}</h3>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
